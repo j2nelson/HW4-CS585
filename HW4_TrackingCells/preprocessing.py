bbee92f0989ee7934d0a1826a37c1fd2c1e27e67
j2nelson <j2nelson@student.bridgew.edu>
2018-10-30 19:09:19 -0400

    Adding visualiztion

--- HW4_TrackingCells/preprocessing.py
+++ HW4_TrackingCells/preprocessing.py
@@ -6,7 +6,7 @@ from os.path import isfile, join, abspath, exists
 def preprocessing():
     # read image paths
     data_path = abspath('./CS585-CellImages/Normalized')
-    data_list = [join(data_path, file) for file in listdir(data_path) if isfile(join(data_path, file)) and 'jpg' in file]
+    data_list = [join(data_path, file) for file in sorted(listdir(data_path)) if isfile(join(data_path, file)) and 'jpg' in file]
 
     # load images
     images = []
--- HW4_TrackingCells/visualization.py
+++ HW4_TrackingCells/visualization.py
@@ -1,16 +1,16 @@
 
-import numpy as np
-import matplotlib
-import math
 import cv2
-from os import listdir
-from os.path import isfile, join, abspath, exists
-
 
 def visualization(tracked_objects, images):
     for img in range(len(images)):
-        cv2.imshow("Hi", images[img])
-        cv2.waitKey(0)
-        
-    cv2.destroyAllWindows()
+        cv2.imwrite("./output/" + str(img) + '.jpeg', images[img])
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    out = cv2.VideoWriter('./output_video.MP4', fourcc, 3.0, (640,480))
+    for output in images:
+        output = cv2.resize(images[img], (640,480))
+        out.write(output)
+    out.release()
+
     return
