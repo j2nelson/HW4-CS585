e01d8d5353f0ac0f38a3fcbc042871c073849bbb
j2nelson <j2nelson@student.bridgew.edu>
2018-10-30 19:41:04 -0400

    adding video output

--- HW4_TrackingCells/visualization.py
+++ HW4_TrackingCells/visualization.py
@@ -1,5 +1,10 @@
 
+import numpy as np
+import matplotlib
+import math
 import cv2
+from os import listdir
+from os.path import isfile, join, abspath, exists
 
 def visualization(tracked_objects, images):
     for img in range(len(images)):
@@ -9,8 +14,10 @@ def visualization(tracked_objects, images):
     fourcc = cv2.VideoWriter_fourcc(*'H264')
     out = cv2.VideoWriter('./output_video.MP4', fourcc, 3.0, (640,480))
     for output in images:
-        output = cv2.resize(images[img], (640,480))
-        out.write(output)
+        output = cv2.resize(output, (640,480))
+        backtorgb = cv2.cvtColor(output,cv2.COLOR_GRAY2BGR)
+        out.write(backtorgb)
+
     out.release()
 
     return
